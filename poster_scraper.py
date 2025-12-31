2f48543296e33c4187254b1eecd462d8a5ba5189
Caleb <67242053+peixeman@users.noreply.github.com>
2025-12-31 15:51:58 -0600

    Add files via upload

---
+++ poster_scraper.py
@@ -0,0 +1,46 @@
+import wikipedia
+
+def find_movie_match(articles, title, year):
+    for article in articles:
+        if f"{title.lower()} ({str(year)} film)" in article.lower():
+            return article
+    for article in articles:
+        if f"{title.lower()} (film)" in article.lower():
+            return article
+    for article in articles:
+        if title.lower() in article.lower() or "film" in article.lower() or str(year) in article.lower():
+            return article
+    return articles[0]
+
+def check_percentage_of_word_matches(img_url, movie_title):
+    match_counter = 0
+    title_words = movie_title.lower().split()
+    for word in title_words:
+        if word in img_url.lower():
+            match_counter += 1
+    return match_counter / len(title_words)
+
+def main(title, year):
+    try:
+        articles = wikipedia.search(title, results=20)
+    except Exception as e:
+        return e
+    else:
+        movie_wiki = wikipedia.page(find_movie_match(articles,title,year),auto_suggest=False)
+
+        for image in movie_wiki.images:
+            if "poster" in image.lower():
+                return image
+        for image in movie_wiki.images:
+            if title.lower() in image.lower():
+                return image
+        matches = []
+        for image in movie_wiki.images:
+            match_percentage = check_percentage_of_word_matches(image, title)
+            matches.append([match_percentage, image])
+        if max(matches, key=lambda x: x[0])[0] <= 0.5:
+            return None
+        return max(matches, key=lambda x: x[0])[1]
+
+if __name__ == "__main__":
+    main(title="",year="")
